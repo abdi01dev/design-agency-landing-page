0773b035caaa73c9378ca1e844564c59efe7f032
abdi01dev <abdi01dev@gmail.com>
2024-02-06 15:41:10 +0700

    feat: menambahkan fungsionalitas pada dropdown

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
 // ======== outline debug ========
 document.body.addEventListener('dblclick', () => {
-  document.body.classList.toggle('debug');
+  // document.body.classList.toggle('debug');
+});
+
+// ======== hover debug ========
+window.addEventListener('mousemove', e => {
+  // console.log(e.target);
+});
+
+// ======== active dropdown ========
+let dropdownOpen = false;
+const dropdown = document.querySelector('.navbar__dropdown');
+const dropdownHeader = dropdown.querySelector('.navbar__dropdown-header');
+const dropdownElements = ['navbar__dropdown-menu', 'navbar__dropdown-item', 'navbar__dropdown-link', 'navbar__link-wrapper'];
+
+// fungsi eventHandler saat meng-klik dan meng-hover
+function dropdownOnClickAndOnHoverEventHandler() {
+  if (dropdownOpen === false) {
+    dropdown.classList.add('open');
+    dropdownOpen = true;
+  } else {
+    dropdown.classList.remove('open');
+    dropdownOpen = false;
+  }
+}
+
+// fungsi eventHandler ketika tidak menghover elemen dropdown
+function dropdownMouseLeaveEventHandler(e) {
+  if (!dropdownElements.includes(e.target.classList.value)) {
+    dropdownOpen = false;
+    dropdown.classList.remove('open');
+    window.removeEventListener('mousemove', dropdownMouseLeaveEventHandler);
+  }
+}
+
+// dropdownHeader saat diklik
+dropdownHeader.addEventListener('click', function () {
+  dropdownOnClickAndOnHoverEventHandler();
+});
+
+// dropdownHeader saat dihover
+dropdownHeader.addEventListener('mouseenter', function () {
+  dropdownOnClickAndOnHoverEventHandler();
+});
+
+// dropdownHeader saat tidak dihover
+dropdownHeader.addEventListener('mouseleave', () => {
+  if (dropdownOpen) {
+    window.addEventListener('mousemove', dropdownMouseLeaveEventHandler);
+  }
 });
